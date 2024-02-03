9763504cd4b331a8c11b0d1acfca101b9a05978d
AndriiSmolinskyi <awful.braid@gmail.com>
2024-02-03 16:11:09 +0200

    1

--- js/script.js
+++ js/script.js
@@ -4,24 +4,46 @@ const closeBtn = document.querySelector('.close');
 
 burgerBtn.addEventListener('click', () => {
     popupHeader.style.display = 'flex';
-    closeBtn.style.display = 'block';
-    burgerBtn.style.display = 'none';
 });
 
 closeBtn.addEventListener('click', () => {
     popupHeader.style.display = 'none';
-    closeBtn.style.display = 'none';
-    burgerBtn.style.display = 'inline-block';
 });
 
 window.addEventListener('resize', () => {
     if (window.innerWidth > 670) {
         popupHeader.style.display = 'flex';
-        closeBtn.style.display = 'none';
-        burgerBtn.style.display = 'none';
     } else {
         popupHeader.style.display = 'none';
-        closeBtn.style.display = 'none';
-        burgerBtn.style.display = 'inline-block';
     }
+});
+
+
+const firModule = document.querySelector('.tl-1')
+const secModule = document.querySelector('.tl-2')
+const thirdModule = document.querySelector('.tl-3')
+
+const firModuleCon = document.querySelector('.cont-1')
+const secModuleCon = document.querySelector('.cont-2')
+const thirdModuleCon = document.querySelector('.cont-3')
+
+function toggleModule(module, moduleCon) {
+    if (moduleCon.style.display === 'flex') {
+        moduleCon.style.display = 'none'; // якщо відображено, то приховати
+    } else {
+        moduleCon.style.display = 'flex'; // якщо приховано, то відобразити
+    }
+}
+
+// Додаємо слухачі подій для кожного модулю
+firModule.addEventListener('click', () => {
+    toggleModule(firModule, firModuleCon);
+});
+
+secModule.addEventListener('click', () => {
+    toggleModule(secModule, secModuleCon);
+});
+
+thirdModule.addEventListener('click', () => {
+    toggleModule(thirdModule, thirdModuleCon);
 });
